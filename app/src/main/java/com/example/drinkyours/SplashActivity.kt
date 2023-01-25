665ba3edaf8b6f5693009da12021cef2115e34f3
Aayushi <aayushitiwari21@navgurukul.org>
2023-01-25 05:32:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/drinkyours/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.drinkyours
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val img:ImageView=findViewById(R.id.image)
+        val txt:TextView=findViewById(R.id.txt)
+
+        img.alpha=0f
+        img.animate().setDuration(2000).alpha(1f).withEndAction {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+}
